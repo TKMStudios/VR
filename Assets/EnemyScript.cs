285adcc53eee9a5285cbfb915d473480213a5551
PrimaryD2 <max.visuri@hotmail.se>
2018-02-02 11:06:01 +0100

    VR 10

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -2,23 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class EnemyScript : MonoBehaviour
 {
 
-    public EnemyPlayers enemyPlayer;
+    public EnemyPlayers enemy1;
+    public EnemyPlayers enemy2;
+    public EnemyPlayers enemy3;
 
-    public Text healthText;
-    
+    public GameObject enemyObject1;
+    public GameObject enemyObject2;
+    public GameObject enemyObject3;
+
+    public TextMeshProUGUI enemyHealth;
+    public TextMeshProUGUI enemyHealth2;
+    public TextMeshProUGUI enemyHealth3;
+
+    public TextMeshProUGUI enemyName1;
+    public TextMeshProUGUI enemyName2;
+    public TextMeshProUGUI enemyName3;
+
     // Use this for initialization
     void Start()
     {
-       
+        enemyName1.text = enemy1.name;
+        enemyName2.text = enemy2.name;
+        enemyName3.text = enemy3.name;  
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthText.text = enemyPlayer.health.ToString();
+        enemyHealth.text = enemy1.health.ToString();
+        enemyHealth2.text = enemy2.health.ToString();
+        enemyHealth3.text = enemy3.health.ToString();
+
+        if (enemy1.health == 0)
+        {
+            Destroy(enemyObject1);
+        }
+        else if (enemy2.health == 0)
+        {
+            Destroy(enemyObject2);
+        }
+        else if (enemy3.health == 0)
+        {
+            Destroy(enemyObject3);
+        }
     }
 }
--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -6,13 +6,26 @@ public class BulletScript : MonoBehaviour
 {
     public GameObject bullet;
     public EnemyPlayers enemyPlayer;
+    public EnemyPlayers enemyPlayer2;
+    public EnemyPlayers enemyPlayer3;
+    public GameObject[] enemys;
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Enemys")
+        if (collision.gameObject.tag == "Enemy1")
         {
             Destroy(bullet);
-            enemyPlayer.health =- 50;
+            enemyPlayer.health -= 50;
+        }
+        else if (collision.gameObject.tag == "Enemy2")
+        {
+            Destroy(bullet);
+            enemyPlayer2.health -= 50;
+        }
+        else if (collision.gameObject.tag == "Enemy3")
+        {
+            Destroy(bullet);
+            enemyPlayer3.health -= 50;
         }
     }
 }
